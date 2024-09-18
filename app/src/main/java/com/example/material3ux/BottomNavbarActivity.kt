297aae08b13bb549f12140ef155c842d93464b74
Eyüp Atıcı <integral173@hotmail.com>
2024-09-18 11:42:39 +0300

    Navigation bar added

--- app/src/main/java/com/example/material3ux/BottomNavbarActivity.kt
+++ app/src/main/java/com/example/material3ux/BottomNavbarActivity.kt
@@ -3,7 +3,6 @@ package com.example.material3ux
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -21,13 +20,17 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.material3ux.ui.theme.Material3UXTheme
+
+// Use Nav bar only if you will have 3 to 5 buttons on the bottom
 
 data class BottomNavigationItem(
     val title: String,
@@ -37,82 +40,95 @@ data class BottomNavigationItem(
     val badgeCount: Int? = null
 )
 
-// Use only if you will have 3 to 5 buttons on the bottom
-
 class BottomNavbarActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Material3UXTheme {
-                val items = listOf(
-                    BottomNavigationItem(
-                        title = "Home",
-                        selectedIcon = Icons.Filled.Home,
-                        unselectedIcon = Icons.Outlined.Home,
-                        hasNews = false,
-                    ),
-                    BottomNavigationItem(
-                        title = "Chat",
-                        selectedIcon = Icons.Filled.MarkEmailUnread,
-                        unselectedIcon = Icons.Outlined.MarkEmailUnread,
-                        hasNews = false,
-                        badgeCount = 3
-                    ),
-                    BottomNavigationItem(
-                        title = "Settings",
-                        selectedIcon = Icons.Filled.Settings,
-                        unselectedIcon = Icons.Outlined.Settings,
-                        hasNews = true,
-                    )
-                )
-                var selectedItemIndex by rememberSaveable {
-                    mutableIntStateOf(0)
-                }
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    bottomBar = {
-                        NavigationBar {
-                            items.forEachIndexed { index, item ->
-                                NavigationBarItem(
-                                    selected = selectedItemIndex == index,
-                                    onClick = {
-                                        selectedItemIndex = index
-                                    },
-                                    label = {
-                                      Text(text = item.title)
-                                    },
-                                    icon = {
-                                        BadgedBox(
-                                            badge = {
-                                                if (item.badgeCount != null) {
-                                                    Badge {
-                                                        Text(text = item.badgeCount.toString())
-                                                    }
-                                                } else if (item.hasNews) {
-                                                    Badge()
-                                                }
-                                            }
-                                        ) {
-                                            Icon(
-                                                imageVector = if (index == selectedItemIndex) {
-                                                    item.selectedIcon
-                                                } else item.unselectedIcon,
-                                                contentDescription = item.title
-                                            )
-                                        }
-                                    })
-                            }
-                        }
-                    }
-                ) {
-                    Column(
-                        modifier = Modifier.padding(it)
-                    ) {
+            BottomNavbarM3()
+        }
+    }
+}
 
+@Composable
+fun BottomNavbarM3() {
+    Material3UXTheme {
+        val items = listOf(
+            BottomNavigationItem(
+                title = "Home",
+                selectedIcon = Icons.Filled.Home,
+                unselectedIcon = Icons.Outlined.Home,
+                hasNews = false
+            ),
+            BottomNavigationItem(
+                title = "Chat",
+                selectedIcon = Icons.Filled.MarkEmailUnread,
+                unselectedIcon = Icons.Outlined.MarkEmailUnread,
+                hasNews = false,
+                badgeCount = 12
+            ),
+            BottomNavigationItem(
+                title = "Settings",
+                selectedIcon = Icons.Filled.Settings,
+                unselectedIcon = Icons.Outlined.Settings,
+                hasNews = true
+            )
+        )
+        var selectedItemIndex by rememberSaveable {
+            mutableIntStateOf(0)
+        }
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize(),
+            bottomBar = {
+                NavigationBar {
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            selected = selectedItemIndex == index,
+                            onClick = {
+                                selectedItemIndex = index
+                            },
+                            icon = {
+                                BadgedBox(
+                                    badge = {
+                                        if (item.badgeCount != null) {
+                                            Badge {
+                                                Text(text = item.badgeCount.toString())
+                                            }
+                                        }
+                                        if (item.hasNews) {
+                                            Badge()
+                                        }
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = if (index == selectedItemIndex) {
+                                            item.selectedIcon
+                                        } else item.unselectedIcon,
+                                        contentDescription = item.title
+                                    )
+                                }
+                            },
+                            label = {
+                                Text(item.title)
+                            }
+                        )
                     }
                 }
             }
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+
+            }
         }
     }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun BottomNavbarM3Preview() {
+    BottomNavbarM3()
 }
